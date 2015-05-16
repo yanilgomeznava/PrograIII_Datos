d55f53a3c8efa98ef434149a33ef3064775a09c9
Luis Alonzo Cascante <luisalonzocascante@gmail.com>
2015-05-15 22:22:18 -0600

    Prueba de dibujar círculos por nodo

--- PrograIII/main.cpp
+++ PrograIII/main.cpp
@@ -16,13 +16,15 @@ int main()
 
     int xActual = 0;
     int yActual = 0;
+    int columnas = 0;
+    int filas = 0;
 
     while(i == 0)
     {
         xActual = X_INICIAL * contador;
         yActual = Y_INICIAL * contador;
-        initwindow(xActual, yActual);
         grafo = new DLinkedList<int>[yActual]();
+
         for(int i = 0; i < xActual; i++)
         {
             for(int j = 0; j < yActual; j++)
@@ -31,8 +33,28 @@ int main()
             }
         }
 
+        initwindow(xActual, yActual);
+        columnas = xActual / X_INICIAL;
+        filas = yActual / Y_INICIAL;
+
+        cout << columnas << " - " << filas << endl;
+
+        int FONDO = COLOR(255, 255, 255);
+        setcolor(FONDO);
+        for(int i = 0; i < filas; i++)
+        {
+            for(int j = 0; j < columnas; j++)
+            {
+                circle(i, j , 5);
+            }
+        }
+
+
+
         cin >> i;
         contador ++;
+        cleardevice();
+        closegraph();
     }
 
     return 0;
