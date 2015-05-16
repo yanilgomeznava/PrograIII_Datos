3ba894675cd02adc49dc28826cd8eba32c65cc23
Luis Alonzo Cascante <luisalonzocascante@gmail.com>
2015-05-15 22:09:49 -0600

    Creación de ventanas
    Las ventanas se crean una vez por ciclo y crecen cada ciclo, se duplica
    el tamaño.

--- PrograIII/main.cpp
+++ PrograIII/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <winbgim.h>
 #include "DLinkedList.h"
 
 using namespace std;
 
 int main()
 {
+    int X_INICIAL = 50;
+    int Y_INICIAL = 50;
+
     DLinkedList<int> *grafo;
 
+    int i=0;
+    int contador = 1;
 
+    int xActual = 0;
+    int yActual = 0;
+
+    while(i == 0)
+    {
+        xActual = X_INICIAL * contador;
+        yActual = Y_INICIAL * contador;
+        initwindow(xActual, yActual);
+        grafo = new DLinkedList<int>[yActual]();
+        for(int i = 0; i < xActual; i++)
+        {
+            for(int j = 0; j < yActual; j++)
+            {
+                grafo[i].insert(j);
+            }
+        }
+
+        cin >> i;
+        contador ++;
+    }
 
     return 0;
 }
