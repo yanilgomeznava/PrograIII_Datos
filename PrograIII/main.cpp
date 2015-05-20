7ac37f273c09305d6b847783fbafd3f4e5431569
Luis Alonzo Cascante <luisalonzocascante@gmail.com>
2015-05-20 08:08:20 -0600

    Error con dibujar en nuevas pantallas
    URGENTE

--- PrograIII/main.cpp
+++ PrograIII/main.cpp
@@ -7,53 +7,36 @@ using namespace std;
 int main()
 {
     int X_INICIAL = 50;
-    int Y_INICIAL = 50;
+    int Y_INICIAL = 25;
 
     DLinkedList<int> *grafo;
 
     int i=0;
     int contador = 1;
 
-    int xActual = 0;
-    int yActual = 0;
+    int columnasActual = 0;
+    int filasActual = 0;
     int columnas = 0;
     int filas = 0;
 
     while(i == 0)
     {
-        xActual = X_INICIAL * contador;
-        yActual = Y_INICIAL * contador;
-        grafo = new DLinkedList<int>[yActual]();
+        columnasActual = X_INICIAL * contador;
+        filasActual = Y_INICIAL * contador;
 
-        for(int i = 0; i < xActual; i++)
-        {
-            for(int j = 0; j < yActual; j++)
-            {
-                grafo[i].insert(j);
-            }
-        }
+        grafo = new DLinkedList<int>[columnasActual * filasActual];
 
-        initwindow(xActual, yActual);
-        columnas = xActual / X_INICIAL;
-        filas = yActual / Y_INICIAL;
-
+        initwindow(columnasActual, filasActual);
+        columnas = columnasActual / X_INICIAL;
+        filas = filasActual / Y_INICIAL;
         cout << columnas << " - " << filas << endl;
 
-        int FONDO = COLOR(255, 255, 255);
-        setcolor(FONDO);
-        for(int i = 0; i < filas; i++)
-        {
-            for(int j = 0; j < columnas; j++)
-            {
-                circle(i, j , 5);
-            }
-        }
+        circle(columnasActual / 2, filasActual / 2, 1);
 
 
-
+        delete [] grafo;
         cin >> i;
         contador ++;
-        cleardevice();
         closegraph();
     }
 
